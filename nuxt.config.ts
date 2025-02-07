807a4c0d986b67d2d4e031a617afa13efa39a8a7
yoonjoohyun <crayon_000@optten.co.kr>
2025-02-07 22:41:22 +0900

    message success

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,15 @@ export default defineNuxtConfig({
           // Remix-icon
           rel: 'stylesheet',
           href: 'https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css'
+        },
+        {
+          // MinSans font
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/gh/poposnail61/min-sans@main/web/css/minsansvf-dynamic-subset.css'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css'
         }
       ]
     }
