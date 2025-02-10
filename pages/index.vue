264c667c8af0afb314121b68fabc5276169fa3bc
yoonjoohyun <crayon_000@optten.co.kr>
2025-02-11 00:57:58 +0900

    section

--- pages/index.vue
+++ pages/index.vue
@@ -1,112 +1,160 @@
 <template>
-<div class="wrap">
-    <div class="content">
-        <div class="topline"><p>♡ 주현이와 영경이의 결혼식에 초대합니다 ♡</p></div>
-        <div class="title_box">
-            <div class="title">YOU & ME 10 YEARS<br>WE TOGETHER 100 YEARS</div>
+<div class="wrap" @wheel="handleWheel" @touchstart="handleTouchStart" @touchend="handleTouchEnd">
+    <div class="sections" :style="{ transform: `translateY(-${currentSection * 100}%)` }">
+        <div class="content section">
+            <div class="topline"><p>♡ 주현이와 영경이의 결혼식에 초대합니다 ♡</p></div>
+            <div class="title_box">
+                <div class="title">YOU & ME 10 YEARS<br>WE TOGETHER 100 YEARS</div>
+            </div>
+            <div class="subtitle"><p class="subtitle_txt">너와 내가 서로 마주보고 행복했던 지난 10년처럼
+                <br>이제는 우리 함께 나란히 서서 앞으로 100년 더 행복하자</p>
+            </div>
+            <div class="maincover">
+                <img src="~/assets/images/maincover.webp" alt="maincover">
+            </div>
+            <div class="people">윤 종문 · 황 인경 의 차남 <span class="b">주현</span></div>
+            <div class="people">오 연배 · 김 은숙 의 차녀 <span class="b">영경</span></div>
+            <div class="date">2023년 8월 23일 토요일 오후 1시 20분</div>
+            <div class="location">강동역 3번출구 KDW웨딩 (도보 1분)</div>
         </div>
-        <div class="subtitle"><p class="subtitle_txt">너와 내가 서로 마주보고 행복했던 지난 10년처럼
-            <br>이제는 우리 함께 나란히 서서 앞으로 100년 더 행복하자</p>
-        </div>
-        <div class="maincover">
-            <img src="~/assets/images/maincover.webp" alt="maincover">
-        </div>
-        <div class="people">윤 종문 · 황 인경 의 차남 <span class="b">주현</span></div>
-        <div class="people">오 연배 · 김 은숙 의 차녀 <span class="b">영경</span></div>
-        <div class="date">2023년 8월 23일 토요일 오후 1시 20분</div>
-        <div class="location">강동역 3번출구 KDW웨딩 (도보 1분)</div>
-        <div class="bottom_space"></div>
-    </div>
-    <div class="content02">
-        <PhotoBox 
-            :is-open="isPhotoBoxOpen" 
-            :images="allImages" 
-            :initial-slide="currentImageIndex"
-            @close="closePhotoBox" 
-        />
-        <div class="photo_box">
-            <div class="photo" @click="openPhotoBox(0)">
-                <img src="~/assets/images/01.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(1)">
-                <img src="~/assets/images/02.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(2)">
-                <img src="~/assets/images/03.webp"/>
-            </div>
-        </div>
-        <div class="photo_box">
-            <div class="photo" @click="openPhotoBox(3)">
-                <img src="~/assets/images/04.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(4)">
-                <img src="~/assets/images/05.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(5)">
-                <img src="~/assets/images/06.webp"/>
-            </div>
-        </div>
-        <div class="photo_box">
-            <div class="photo" @click="openPhotoBox(6)">
-                <img src="~/assets/images/07.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(7)">
-                <img src="~/assets/images/08.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(8)">
-                <img src="~/assets/images/09.webp"/>
-            </div>
-        </div>
-        <div class="photo_box">
-            <div class="photo" @click="openPhotoBox(9)">
-                <img src="~/assets/images/10.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(10)">
-                <img src="~/assets/images/11.webp"/>
-            </div>
-            <div class="photo" @click="openPhotoBox(11)">
-                <img src="~/assets/images/12.webp"/>
-            </div>
-        </div>
-        <div class="empty"></div>
-    </div>
-    <div class="content03">
-        <div class="map_title">
-            찾아오시는 길
-        </div>
-        <div class="map">
-            <div class="kakaomap">
-                <img src="http://t1.daumcdn.net/roughmap/imgmap/954a38ecc459fc1f97b5a423807bec77e66b8d466f406c51125f87d31e4080d8">
-            </div>
-            <div class="map_navi">
-                <div class="left">
-                    <a class="kakao_logo" href="https://map.kakao.com" target="_blank"><img src="//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/logo_kakaomap.png"></a>
+        <div class="content02 section">
+            <PhotoBox 
+                :is-open="isPhotoBoxOpen" 
+                :images="allImages" 
+                :initial-slide="currentImageIndex"
+                @close="closePhotoBox" 
+            />
+            <div class="bottom_space"></div>
+            <div class="photo_box">
+                <div class="photo" @click="openPhotoBox(0)">
+                    <img src="~/assets/images/01.webp"/>
                 </div>
-                <div class="right">
-                    <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;srcid=10124741&amp;confirmid=10124741&amp;q=KDW%EC%9B%A8%EB%94%A9&amp;rv=on">로드뷰</a>
-                    <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;eName=KDW%EC%9B%A8%EB%94%A9&amp;eX=529548.0&amp;eY=1120995.0">길찾기</a>
-                    <a target="_blank" href="https://map.kakao.com/?urlX=529548.0&amp;urlY=1120995.0&amp;itemId=10124741&amp;q=KDW%EC%9B%A8%EB%94%A9&amp;srcid=10124741&amp;map_type=TYPE_MAP&amp;from=roughmap">
-                        자세히
-                    </a> 
+                <div class="photo" @click="openPhotoBox(1)">
+                    <img src="~/assets/images/02.webp"/>
+                </div>
+                <div class="photo" @click="openPhotoBox(2)">
+                    <img src="~/assets/images/03.webp"/>
                 </div>
             </div>
+            <div class="photo_box">
+                <div class="photo" @click="openPhotoBox(3)">
+                    <img src="~/assets/images/04.webp"/>
+                </div>
+                <div class="photo" @click="openPhotoBox(4)">
+                    <img src="~/assets/images/05.webp"/>
+                </div>
+                <div class="photo" @click="openPhotoBox(5)">
+                    <img src="~/assets/images/06.webp"/>
+                </div>
+            </div>
+            <div class="photo_box">
+                <div class="photo" @click="openPhotoBox(6)">
+                    <img src="~/assets/images/07.webp"/>
+                </div>
+                <div class="photo" @click="openPhotoBox(7)">
+                    <img src="~/assets/images/08.webp"/>
+                </div>
+                <div class="photo" @click="openPhotoBox(8)">
+                    <img src="~/assets/images/09.webp"/>
+                </div>
+            </div>
+            <div class="photo_box">
+                <div class="photo" @click="openPhotoBox(9)">
+                    <img src="~/assets/images/10.webp"/>
+                </div>
+                <div class="photo" @click="openPhotoBox(10)">
+                    <img src="~/assets/images/11.webp"/>
+                </div>
+                <div class="photo" @click="openPhotoBox(11)">
+                    <img src="~/assets/images/12.webp"/>
+                </div>
+            </div>
+            <div class="empty"></div>
         </div>
-        <div class="explain">
-            <div class="vehicle">버스</div>
-            <div class="detail">
-                <p class="blue">파랑 340번, 364번, 240번</p>
-                <p class="green">초록 1180번, 1124번</p>
-                <p class="yellow">마을 02번, 05번</p>
+        <div class="content03 section">
+            <div class="map_title">
+                찾아오시는 길
+            </div>
+            <div class="map">
+                <div class="kakaomap">
+                    <img src="http://t1.daumcdn.net/roughmap/imgmap/954a38ecc459fc1f97b5a423807bec77e66b8d466f406c51125f87d31e4080d8">
+                </div>
+                <div class="map_navi">
+                    <div class="left">
+                        <a class="kakao_logo" href="https://map.kakao.com" target="_blank"><img src="//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/logo_kakaomap.png"></a>
+                    </div>
+                    <div class="right">
+                        <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;srcid=10124741&amp;confirmid=10124741&amp;q=KDW%EC%9B%A8%EB%94%A9&amp;rv=on">로드뷰</a>
+                        <a target="_blank" href="https://map.kakao.com/?from=roughmap&amp;eName=KDW%EC%9B%A8%EB%94%A9&amp;eX=529548.0&amp;eY=1120995.0">길찾기</a>
+                        <a target="_blank" href="https://map.kakao.com/?urlX=529548.0&amp;urlY=1120995.0&amp;itemId=10124741&amp;q=KDW%EC%9B%A8%EB%94%A9&amp;srcid=10124741&amp;map_type=TYPE_MAP&amp;from=roughmap">
+                            자세히
+                        </a> 
+                    </div>
+                </div>
+            </div>
+            <div class="explain">
+                <div class="vehicle">버스</div>
+                <div class="detail">
+                    <p class="blue">파랑 340번, 364번, 240번</p>
+                    <p class="green">초록 1180번, 1124번</p>
+                    <p class="yellow">마을 02번, 05번</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="content04">
-        <Guestbook/>
+        <div class="content04 section">
+            <Guestbook/>
+        </div>
     </div>
 </div>
 </template>
 
 <script setup>
+const currentSection = ref(0);
+const touchStartY = ref(0);
+const totalSections = 4;
+let isAnimating = false;
+
+const changeSection = (direction) => {
+    if (isAnimating) return;
+    
+    isAnimating = true;
+    if (direction === 'down' && currentSection.value < totalSections - 1) {
+        currentSection.value++;
+    } else if (direction === 'up' && currentSection.value > 0) {
+        currentSection.value--;
+    }
+    
+    setTimeout(() => {
+        isAnimating = false;
+    }, 1000); // 애니메이션 시간과 동일하게 설정
+};
+
+const handleWheel = (e) => {
+    if (e.deltaY > 0) {
+        changeSection('down');
+    } else {
+        changeSection('up');
+    }
+};
+
+const handleTouchStart = (e) => {
+    touchStartY.value = e.touches[0].clientY;
+};
+
+const handleTouchEnd = (e) => {
+    const touchEndY = e.changedTouches[0].clientY;
+    const diff = touchStartY.value - touchEndY;
+    
+    if (Math.abs(diff) > 50) { // 50px 이상 스와이프했을 때만 동작
+        if (diff > 0) {
+            changeSection('down');
+        } else {
+            changeSection('up');
+        }
+    }
+};
+
+
 import PhotoBox from '~/components/PhotoBox.vue';
 import { ref } from 'vue';
 import img1 from '~/assets/images/01.webp';
